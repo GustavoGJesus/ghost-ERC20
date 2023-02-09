8b1da929fcce8bb6cb5c3cf48847d2a8fd7e0acb
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-02-09 20:22:44 -0300

    feat: adjust in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const connectContract =  async () => {
+    const Address = "0xc3bE07cdc95AdB00527FbA5754EA4f463c4911Ef";
+    // abi 
+    return;
+  }
   return (
     <div className={styles.container}>
-      Home
+      <button>deployed</button>
     </div>
-  )
+  );
 }
