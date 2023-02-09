7c31a2df3fbd2994b11dfd3f0b5f32466a4a36fb
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-02-09 20:21:21 -0300

    feat: add goerli network for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: process.env.GOERLI_PRIVATE_KEY,
+    },
+  },
 };
